08a9036dff93f2a45142ee1fda9cf3906eba875c
maszrum <grzyb3k@hotmail.com>
2024-03-18 12:48:08 +0100

    Add System.CommandLine

---
+++ CryptoLooser.Cli/Commands/LearnCommand.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Immutable;
+using System.CommandLine;
+using CryptoLooser.Core;
+using CryptoLooser.Core.Mendel;
+using CryptoLooser.Core.Parsing;
+using Serilog;
+
+namespace CryptoLooser.Cli.Commands;
+
+internal class LearnCommand : Command
+{
+    private const string CommandName = "learn";
+    private const string CommandDescription = "Start learning neural network with generic algorithm.";
+
+    private readonly ILogger _logger;
+    private readonly NeuralNetworkSettings _neuralNetworkSettings;
+    private readonly GeneticAlgorithmSettings _geneticAlgorithmSettings;
+
+    public LearnCommand(
+        ILogger logger,
+        NeuralNetworkSettings neuralNetworkSettings,
+        GeneticAlgorithmSettings geneticAlgorithmSettings)
+        : base(CommandName, CommandDescription)
+    {
+        _logger = logger;
+        _neuralNetworkSettings = neuralNetworkSettings;
+        _geneticAlgorithmSettings = geneticAlgorithmSettings;
+
+        this.SetHandler(context => Handle(context.GetCancellationToken()));
+    }
+
+    private async Task Handle(CancellationToken cancellationToken)
+    {
+        var marketFilesLocation = Path.Combine("market-data", "ethusdt");
+
+        _logger.Information(
+            "Reading and parsing market data files from location: {MarketFilesLocation}",
+            marketFilesLocation);
+
+        var parser = BinanceMarketDataFileParser.Create("ETHUSDT", marketFilesLocation);
+
+        MarketDataRow[] marketData;
+        try
+        {
+            marketData = await parser
+                .GetMarketData(cancellationToken)
+                .ToArrayAsync(cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            return;
+        }
+
+        _logger.Information(
+            "Loaded {MarketDataRowsCount} market data rows.",
+            marketData.Length);
+
+        var marketSimulation = new MarketSimulation(
+            marketData: marketData.ToImmutableArray(),
+            neuralNetworkHiddenLayerNeuronsCount: _neuralNetworkSettings.HiddenLayerNeuronsCount,
+            neuralNetworkSeriesSize: _neuralNetworkSettings.SeriesSize);
+
+        var chromosomeFactory = new RandomChromosomeFactory(marketSimulation.RequiredChromosomeLength);
+
+        var geneticAlgorithmParams = new GeneticAlgorithmParams
+        {
+            PopulationSize = _geneticAlgorithmSettings.PopulationSize,
+            InsertBestIndividualGenerationInterval = _geneticAlgorithmSettings.InsertBestIndividualGenerationInterval
+        };
+
+        _logger.Information(
+            "Creating initial population with number of {IndividualsCount} individuals.",
+            geneticAlgorithmParams.PopulationSize);
+
+        var geneticAlgorithm = new GeneticAlgorithm<IndividualState>(
+            geneticAlgorithmParams,
+            chromosomeFactory: chromosomeFactory,
+            mutator: new RandomMutator(
+                chromosomeFactory,
+                _geneticAlgorithmSettings.MutationProbability,
+                _geneticAlgorithmSettings.MaxMutationPercentage),
+            fitnessProvider: marketSimulation);
+
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return;
+        }
+
+        _logger.Information(
+            "Best individual in initial population. Fitness: {Fitness}, profit: {Profit}, number of decisions: {DecisionsCount}.",
+            geneticAlgorithm.BestEver.Fitness,
+            geneticAlgorithm.BestEver.State.Profit,
+            geneticAlgorithm.BestEver.State.Decisions.Length);
+
+        var exporter = new ChromosomeExporter(Directory.GetCurrentDirectory(), _neuralNetworkSettings);
+        exporter.Exported += chromosomeFileName =>
+        {
+            _logger.Information(
+                "Chromosome exported to file: {ChromosomeFileName}.",
+                chromosomeFileName);
+        };
+
+        await exporter.Export(geneticAlgorithm.BestEver.GetGenes());
+
+        geneticAlgorithm.BestEverChanged += async individual =>
+        {
+            _logger.Information(
+                "New best individual found. Fitness: {Fitness}, profit: {Profit}, number of decisions: {DecisionsCount}.",
+                individual.Fitness,
+                individual.State.Profit,
+                individual.State.Decisions.Length);
+
+            await exporter.Export(geneticAlgorithm.BestEver.GetGenes());
+        };
+
+        var performanceCounter = new PeriodicPerformanceCounter(
+            TimeSpan.FromSeconds(10),
+            generationsPerSecond =>
+            {
+                _logger.Information(
+                    "Current generation: {GenerationNumber}. Processing {GenerationsPerSecond} generations/s.",
+                    geneticAlgorithm.Generation,
+                    generationsPerSecond);
+            });
+
+        var counterTask = performanceCounter.Start(cancellationToken);
+
+        _logger.Information("Starting iterating through generations...");
+
+        while (
+            !cancellationToken.IsCancellationRequested &&
+            geneticAlgorithm.Generation < _geneticAlgorithmSettings.MaxGenerations)
+        {
+            await geneticAlgorithm.NextGeneration();
+            performanceCounter.Increment();
+        }
+
+        await counterTask;
+
+    }
+}
--- CryptoLooser.Cli/Program.cs
+++ CryptoLooser.Cli/Program.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections.Immutable;
+﻿using System.CommandLine;
 using CryptoLooser.Cli;
-using CryptoLooser.Core;
-using CryptoLooser.Core.Mendel;
-using CryptoLooser.Core.Parsing;
+using CryptoLooser.Cli.Commands;
 using Microsoft.Extensions.Configuration;
 using Serilog;
-
-var programCts = new CancellationTokenSource();
-
-Console.CancelKeyPress += (_, eventArgs) =>
-{
-    programCts.Cancel();
-    eventArgs.Cancel = true;
-};
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appSettings.json")
@@ -30,109 +20,7 @@ var logger = new LoggerConfiguration()
     .ReadFrom.Configuration(configuration)
     .CreateLogger();
 
-var marketFilesLocation = Path.Combine("market-data", "ethusdt");
+var rootCommand = new RootCommand("Bunch of commands to lose money on cryptocurrency market.");
+rootCommand.AddCommand(new LearnCommand(logger, neuralNetworkSettings, geneticAlgorithmSettings));
 
-logger.Information(
-    "Reading and parsing market data files from location: {MarketFilesLocation}",
-    marketFilesLocation);
-
-var parser = BinanceMarketDataFileParser.Create("ETHUSDT", marketFilesLocation);
-
-MarketDataRow[] marketData;
-try
-{
-    marketData = await parser
-        .GetMarketData(programCts.Token)
-        .ToArrayAsync(programCts.Token);
-}
-catch (OperationCanceledException)
-{
-    return;
-}
-
-
-logger.Information(
-    "Loaded {MarketDataRowsCount} market data rows.",
-    marketData.Length);
-
-var marketSimulation = new MarketSimulation(
-    marketData: marketData.ToImmutableArray(),
-    neuralNetworkHiddenLayerNeuronsCount: neuralNetworkSettings.HiddenLayerNeuronsCount,
-    neuralNetworkSeriesSize: neuralNetworkSettings.SeriesSize);
-
-var chromosomeFactory = new RandomChromosomeFactory(marketSimulation.RequiredChromosomeLength);
-
-var geneticAlgorithmParams = new GeneticAlgorithmParams
-{
-    PopulationSize = geneticAlgorithmSettings.PopulationSize,
-    InsertBestIndividualGenerationInterval = geneticAlgorithmSettings.InsertBestIndividualGenerationInterval
-};
-
-logger.Information(
-    "Creating initial population with number of {IndividualsCount} individuals.",
-    geneticAlgorithmParams.PopulationSize);
-
-var geneticAlgorithm = new GeneticAlgorithm<IndividualState>(
-    geneticAlgorithmParams,
-    chromosomeFactory: chromosomeFactory,
-    mutator: new RandomMutator(
-        chromosomeFactory,
-        geneticAlgorithmSettings.MutationProbability,
-        geneticAlgorithmSettings.MaxMutationPercentage),
-    fitnessProvider: marketSimulation);
-
-if (programCts.Token.IsCancellationRequested)
-{
-    return;
-}
-
-logger.Information(
-    "Best individual in initial population. Fitness: {Fitness}, profit: {Profit}, number of decisions: {DecisionsCount}.",
-    geneticAlgorithm.BestEver.Fitness,
-    geneticAlgorithm.BestEver.State.Profit,
-    geneticAlgorithm.BestEver.State.Decisions.Length);
-
-var exporter = new ChromosomeExporter(Directory.GetCurrentDirectory(), neuralNetworkSettings);
-exporter.Exported += chromosomeFileName =>
-{
-    logger.Information(
-        "Chromosome exported to file: {ChromosomeFileName}.",
-        chromosomeFileName);
-};
-
-await exporter.Export(geneticAlgorithm.BestEver.GetGenes());
-
-geneticAlgorithm.BestEverChanged += async individual =>
-{
-    logger.Information(
-        "New best individual found. Fitness: {Fitness}, profit: {Profit}, number of decisions: {DecisionsCount}.",
-        individual.Fitness,
-        individual.State.Profit,
-        individual.State.Decisions.Length);
-
-    await exporter.Export(geneticAlgorithm.BestEver.GetGenes());
-};
-
-var performanceCounter = new PeriodicPerformanceCounter(
-    TimeSpan.FromSeconds(10),
-    generationsPerSecond =>
-    {
-        logger.Information(
-            "Current generation: {GenerationNumber}. Processing {GenerationsPerSecond} generations/s.",
-            geneticAlgorithm.Generation,
-            generationsPerSecond);
-    });
-
-var counterTask = performanceCounter.Start(programCts.Token);
-
-logger.Information("Starting iterating through generations...");
-
-while (
-    !programCts.Token.IsCancellationRequested &&
-    geneticAlgorithm.Generation < geneticAlgorithmSettings.MaxGenerations)
-{
-    await geneticAlgorithm.NextGeneration();
-    performanceCounter.Increment();
-}
-
-await counterTask;
+await rootCommand.InvokeAsync(args);
